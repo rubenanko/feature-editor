f01ca7bd7bb2c6f00489d51095e245a3aa5f5e96
rubenanko <rubenanko@gmail.com>
2025-07-20 21:22:01 +0200

    feat(): arborescence de steps

--- src/component/Step.jsx
+++ src/component/Step.jsx
@@ -1,12 +1,23 @@
+import { useRef,useState } from 'react';
 import Select from 'react-select';
+import data from '../data.json';
+
+function format(stepData)
+{
+    const regexp = new RegExp(/^(.*)"(.*)"(.*)$/)
+    const matchedData = Object.keys(stepData)[0].match(regexp)
+    return [{label:<div>{matchedData[1]} <Select style={{"z-index":"5"}} /> {matchedData[3]}</div>}]
+}
 
 function Step(){
     const stepTypes = [{label:"Quand",value:"WHEN"},{label:"Et que",value:"WHEN"},{label:"Alors",value:"THEN"},{label:"Et",value:"THEN"},{label:"Etant donné que",value:"GIVEN"},]
-
+    const [stepOptions,setStepOptions] = useState([])
+    
     return (
         <div style={{"display": "flex"}}>
-            <Select placeholder="Type" options={stepTypes}/>
-            <Select />
+            <Select onChange={(value) => {setStepOptions(format(data[value.value]))}} placeholder="Type" options={stepTypes}/>
+            {/* <Select onChange={(value) => {setStepOptions(Object.keys(data[value.value]).map(value => {return {"label" : value}}))}} placeholder="Type" options={stepTypes}/> */}
+            <Select options={stepOptions} />
         </div>
     )
 }
